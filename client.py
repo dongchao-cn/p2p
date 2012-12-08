86392db16594d0ebbad80a875dbda11306e93d5d
DC <dccrazyboy@gmail.com>
2012-12-08 13:04:59 +0800

    cons版完成，没界面
    看看wxWidgets，简单的话就做个界面

---
+++ client.py
@@ -0,0 +1,168 @@
+# -*- coding: UTF-8 -*-
+import socket
+import struct
+import os
+import thread
+import random
+
+
+class RequsetType:
+    userReg = '00'
+    userLogin = '01'
+    userLogout = '02'
+    shareFile = '10'
+    getFile = '11'
+class AnswerType:
+    success = '00'
+    failed = '01'
+
+    
+class Client:
+    ServerIP = ''
+    ServerPort = 0
+    sock = None
+    sockListen = None
+    getFileSock = None
+
+    sendDir = u'E:\\p2p\\send\\'
+    downloadDir = u'E:\\p2p\\down\\'
+    
+    def __init__(self):
+        thread.start_new_thread(self.__listenForOtherClient,())
+        pass
+    
+    def __listenForOtherClient(self):
+        ''' 监听等待其他Client的连接请求 '''
+
+        self.sockListen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        localIP = socket.gethostbyname(socket.gethostname())
+        port = random.randint(7000, 7999)
+        #print "local ip:%s %s" % (localIP,port)
+        self.sockListen.bind((localIP, port))
+        self.sockListen.listen(10)
+        
+        while True:
+            connection,address = self.sockListen.accept()
+            # 这里接收到文件名
+            fileName = connection.recv(25)
+            # 发送文件
+            # 对文件分块，一次4K
+            fp = open(self.sendDir + fileName,'rb')
+            while 1:
+                filedata = fp.read(1024 * 4)
+                if not filedata:
+                    break
+                connection.send(filedata)
+            connection.close()
+
+        #thread.exit_thread()
+        
+    def __connectServer(self,ServerIP,ServerPort = 8000):
+        ''' 连接服务器，每次请求就需要连接一次 '''
+        self.ServerIP = ServerIP
+        self.ServerPort = ServerPort
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.ServerIP, self.ServerPort))
+        
+    def __Request(self,reqType,reqContext):
+        localIP = socket.gethostbyname(socket.gethostname())
+        self.__connectServer(localIP);
+        self.sock.send(reqType)
+        self.sock.send(reqContext)
+        ansType = self.sock.recv(2)
+        asnContext = self.sock.recv(1024)
+        return ansType,asnContext
+
+    def userReg(self,Name,Passwd):
+        ''' 用户注册 ''' 
+        reqContext = struct.pack('!25s25s',Name,Passwd)
+        #print reqContext
+        ansType,asnContext = self.__Request(RequsetType.userReg,reqContext)
+        if ansType != AnswerType.success:
+            print 'userReg failed: %s' % asnContext
+        else:
+            print 'userReg success!'
+
+    def userLogin(self,Name,Passwd):
+        ''' 用户登录，返回值为用户ID '''
+        ClientIP = self.sock.getsockname()[0]
+        ClientPort = self.sock.getsockname()[1]
+        listenPort = self.sockListen.getsockname()[1]
+        reqContext = struct.pack('!25s25s25s2I',Name,Passwd,ClientIP,ClientPort,listenPort)
+        ansType,ansContext = self.__Request(RequsetType.userLogin,reqContext)
+        if ansType != AnswerType.success:
+            print 'userLogin failed: %s' % ansContext
+            return False
+        else:
+            ID, = struct.unpack('!I',ansContext)
+            print 'userLogin success: ID = %s' % ID
+            return ID
+
+    def userLogout(self,Name,Passwd):
+        ''' 用户注销 '''
+        reqContext = struct.pack('!25s25s',Name,Passwd)
+        ansType,asnContext = self.__Request(RequsetType.userLogout,reqContext)
+        if ansType != AnswerType.success:
+            print 'userLogout failed: %s' % asnContext
+        else:
+            print 'userLogout success!'
+
+    def shareFile(self,Name,UploadUserID):
+        ''' 分享文件 '''
+        statinfo = os.stat(self.sendDir + Name)
+        reqContext = struct.pack('!25s2I',Name,statinfo.st_size,UploadUserID)
+        
+        ansType,asnContext = self.__Request(RequsetType.shareFile,reqContext)
+        if ansType != AnswerType.success:
+            print 'shareFile failed: %s' % asnContext
+        else:
+            print 'shareFile success!'
+
+    def getFile(self,Name,SaveDir = downloadDir):
+        ''' 查找文件 '''
+        reqContext = struct.pack('!25s',Name)
+        ansType,asnContext = self.__Request(RequsetType.getFile,reqContext)
+        if ansType != AnswerType.success:
+            print 'getFile failed: %s' % asnContext
+        else:
+            Size,DisIP,DisPort = struct.unpack('!I25sI',asnContext)
+            DisIP = DisIP.rstrip('\0')
+            self.__getFile(Name,Size,DisIP,DisPort)
+            print 'getFile success! %s' %Name 
+            
+            
+    def __getFile(self,fileName,fileSize,DisIP,DisPort):
+        self.getFileSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.getFileSock.connect((DisIP, DisPort))
+        # 发送文件名
+        self.getFileSock.send(fileName)
+        restsize = fileSize
+        # 接受文件
+        # 对文件分块，一次4K
+        fp = open(self.downloadDir + fileName,'wb')
+        while 1:
+            if restsize > 1024 * 4:
+                filedata = self.getFileSock.recv(1024 * 4)
+            else:
+                filedata = self.getFileSock.recv(restsize)
+            fp.write(filedata)
+            restsize = restsize-len(filedata)
+            if restsize == 0:
+                break
+
+if __name__ == '__main__':
+    client = Client()
+    #client.listenForOtherClient();
+    #client.Request('ddddd')
+    client.userReg('dc','123')
+    ID = client.userLogin('dc','123')
+
+    client.shareFile('osrloaderv30.zip',ID)
+
+    #os.system("pause")
+    
+    client.getFile('osrloaderv30.zip')
+
+    client.userLogout('dc','123')
+
+
---
+++ server.py
@@ -0,0 +1,333 @@
+# -*- coding: UTF-8 -*-
+import socket
+import sys
+import MySQLdb as mdb
+import string
+import struct
+
+class RequsetType:
+    userReg = '00'
+    userLogin = '01'
+    userLogout = '02'
+    shareFile = '10'
+    getFile = '11'
+class AnswerType:
+    success = '00'
+    failed = '01'
+
+class Server():
+
+    # 数据库的设置
+    dbHost = ''
+    dbPort = 0
+    dbUser = ''
+    dbPasswd = ''
+    dbName = ''
+    
+    AliveUserID = [];
+
+    def __init__(self,dbHost = '127.0.0.1',dbPort = 3306,dbUser = 'root',dbPasswd = '1129',dbName = 'p2p'):
+        self.dbHost = dbHost
+        self.dbPort = dbPort
+        self.dbUser = dbUser
+        self.dbPasswd = dbPasswd
+        self.dbName = dbName
+        
+    def initServer(self,dbName = 'p2p'):
+        ''' 初始化Server数据库，会重置数据库内所有表 '''
+        try:
+            con = None
+            self.dbName = dbName;
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 删除原表
+            cur.execute("DROP TABLE IF EXISTS File;")
+            cur.execute("DROP TABLE IF EXISTS User;")
+            
+            # 创建新表
+            cur.execute("CREATE TABLE User(ID INT PRIMARY KEY AUTO_INCREMENT,Name VARCHAR(25) NOT NULL UNIQUE,\
+                Passwd VARCHAR(25) NOT NULL,IP VARCHAR(25),Port INT,ListenPort INT);")
+            cur.execute("CREATE TABLE File(ID INT PRIMARY KEY AUTO_INCREMENT,Name VARCHAR(25) NOT NULL,Size INT NOT NULL,\
+                UploadUserID INT,FOREIGN KEY(UploadUserID) REFERENCES User(ID) ON DELETE SET NULL ON UPDATE CASCADE);")
+
+            print "initServer Success,All Table Reset!"
+        finally:
+            if con:
+                con.close()
+                
+    def addUser(self,userName,passwd):
+        ''' 添加新用户 '''
+        try:
+            con = None
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 添加新用户
+            val = [userName,passwd]
+            cur.execute("INSERT INTO User(Name,Passwd) VALUES(%s,%s);",val)
+            con.commit()
+            print "addUser Success,Name = %s,Passwd = %s!" % (userName,passwd)
+            return True
+        except:
+            return False
+        finally:
+            if con:
+                con.close()
+
+    def delUser(self,userName):
+        ''' 删除用户 '''
+        try:
+            con = None
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 删除用户
+            val = [userName]
+            cur.execute("DELETE FROM User WHERE Name = %s;",val)
+            con.commit()
+            print "delUser Success,Name = %s!" % userName
+            return True
+        except:
+            return False
+        finally:
+            if con:
+                con.close()
+    def userLogin(self,userName,passwd,IP,Port,listenPort):
+        ''' 用户登陆 '''
+        try:
+            con = None
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 用户登陆
+            val = [userName,passwd]
+            cur.execute("SELECT * FROM User WHERE Name = %s AND Passwd = %s;",val)
+            user = cur.fetchone()
+            val = [IP,Port,listenPort,user[0]]
+            cur.execute("UPDATE User SET IP = %s ,Port = %s ,ListenPort = %s WHERE ID = %s;",val)
+            con.commit()
+            print "userLogin Success,Name = %s,passwd = %s,IP = %s,Port = %s,ListenPort = %s!" % (userName,passwd,IP,Port,listenPort)
+            return user[0]
+        except:
+            return False
+        finally:
+            if con:
+                con.close()
+
+    def userLogout(self,userName,passwd):
+        ''' 用户注销 '''
+        try:
+            con = None
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 用户注销
+            val = [userName,passwd]
+            cur.execute("SELECT * FROM User WHERE Name = %s AND Passwd = %s;",val)
+            user = cur.fetchone()
+            val = ['NULL','0','0',user[0]]
+            cur.execute("UPDATE User SET IP = %s ,Port = %s ,ListenPort = %s WHERE ID = %s;",val)
+            con.commit()
+            print "userLogout Success,Name = %s,passwd = %s!" % (userName,passwd)
+            return True
+        #except:
+            #return False
+        finally:
+            if con:
+                con.close()
+                
+    def getUser(self,UserID):
+        ''' 获得一个用户信息 '''
+        try:
+            con = None
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 获得用户信息
+            val = [UserID]
+            cur.execute("SELECT * FROM User WHERE ID = %s;",val)
+            user = cur.fetchone()
+            print "getUser Success,Name = %s,Passwd = %s,IP = %s,Port = %s,listenPort = %s!" % (user[1],user[2],user[3],user[4],user[5])
+            return user[1],user[2],user[3],user[4],user[5]
+        finally:
+            if con:
+                con.close()
+                
+    def addFile(self,fileName,fileSize,uploadUserID):
+        ''' 共享一个文件 '''
+        try:
+            con = None
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 共享文件
+            val = [fileName,fileSize,uploadUserID]
+            cur.execute("INSERT INTO File(Name,Size,UploadUserID) VALUES(%s,%s,%s);",val)
+            con.commit()
+            print "addFile Success,Name = %s,Size = %s,uploadUserID = %s!" % (fileName,fileSize,uploadUserID)
+            return True
+        except:
+            return False
+        finally:
+            if con:
+                con.close()
+
+    def delFile(self,fileName,uploadUserID):
+        ''' 删除一个文件 '''
+        try:
+            con = None
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 删除文件
+            val = [fileName,uploadUserID]
+            cur.execute("SELECT * FROM File WHERE Name = %s AND UploadUserID = %s;",val)
+            thefile = cur.fetchone()
+            val = [thefile[0]]
+            cur.execute("DELETE FROM File WHERE ID = %s;",val)
+            con.commit()
+            print "delFile Success,Name = %s,uploadUserID = %s!" % (fileName,uploadUserID)
+        finally:
+            if con:
+                con.close()
+                
+    def getFile(self,fileName):
+        ''' 获得一个文件 '''
+        try:
+            con = None
+            con = mdb.connect(host = self.dbHost,port = self.dbPort,user = self.dbUser,\
+                              passwd = self.dbPasswd,db = self.dbName,charset='utf8')
+            cur = con.cursor()
+
+            # 获得文件
+            val = [fileName]
+            cur.execute("SELECT * FROM File WHERE Name = %s;",val)
+            thefile = cur.fetchone()
+            if cur.rowcount == 0:
+                return '',0,0
+            print "getFile Success,Name = %s,Size = %s,uploadUserID = %s!" % (thefile[1],thefile[2],thefile[3])
+            return thefile[1],thefile[2],thefile[3]
+        except:
+            return '',0,0
+        finally:
+            if con:
+                con.close()
+                
+    def startServer(self,ip,port = 8000,maxlisten = 10):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((ip, port))
+        sock.listen(maxlisten)
+        while True:
+            connection,address = sock.accept()
+            reqType = connection.recv(2)
+            reqContext = connection.recv(1024)
+            ansType,ansContext = self.doRequest(reqType,reqContext)
+            connection.send(ansType)
+            connection.send(ansContext)
+            connection.close()
+
+    def doRequest(self,reqType,reqContext):
+        #print reqType
+        #print reqContext
+        if reqType == RequsetType.userReg:
+            # 注册用户
+            Name,Passwd = struct.unpack('!25s25s',reqContext)
+            Name = Name.rstrip('\0')
+            Passwd = Passwd.rstrip('\0')
+            #print Name,Passwd
+            ansContext = ''
+            if self.addUser(Name,Passwd):
+                ansType = AnswerType.success
+            else:
+                ansType = AnswerType.failed
+                ansContext = struct.pack('!100s','RegUser Failed!!!!!!!')
+            return ansType, ansContext
+        
+        if reqType == RequsetType.userLogin:
+            # 登录用户
+            Name,Passwd,IP,Port,listenPort = struct.unpack('!25s25s25s2I',reqContext)
+            Name = Name.rstrip('\0')
+            Passwd = Passwd.rstrip('\0')
+            IP = IP.rstrip('\0')
+            ansContext = ''
+            ret = self.userLogin(Name,Passwd,IP,Port,listenPort)
+            if ret:
+                ansType = AnswerType.success
+                ansContext = struct.pack('!I',ret)
+            else:
+                ansType = AnswerType.failed
+                ansContext = struct.pack('!100s','userlogin Failed!!!!!!!')
+            return ansType, ansContext
+
+        if reqType == RequsetType.userLogout:
+            # 注销用户
+            Name,Passwd = struct.unpack('!25s25s',reqContext)
+            Name = Name.rstrip('\0')
+            Passwd = Passwd.rstrip('\0')
+            ansContext = ''
+            ret = self.userLogout(Name,Passwd)
+            if ret:
+                ansType = AnswerType.success
+            else:
+                ansType = AnswerType.failed
+                ansContext = struct.pack('!100s','userLogout Failed!!!!!!!')
+            return ansType, ansContext
+
+        if reqType == RequsetType.shareFile:
+            # 分享文件
+            Name,Size,UploadUserID = struct.unpack('!25s2I',reqContext)
+            Name = Name.rstrip('\0')
+            ansContext = ''
+            ret = self.addFile(Name,Size,UploadUserID)
+            if ret:
+                ansType = AnswerType.success
+            else:
+                ansType = AnswerType.failed
+                ansContext = struct.pack('!100s','shareFile Failed!!!!!!!')
+            return ansType, ansContext
+
+        if reqType == RequsetType.getFile:
+            # 查找文件
+            Name, = struct.unpack('!25s',reqContext)
+            Name = Name.rstrip('\0')
+            ansContext = ''
+            (a,Size,UploadUserID) = self.getFile(Name)
+            if UploadUserID == 0:
+                ansType = AnswerType.failed
+                ansContext = struct.pack('!100s','No Such File!!!!!!!')
+            else:
+                (a,b,IP,d,ListenPort) = self.getUser(UploadUserID)
+                if IP:
+                    ansType = AnswerType.success
+                    ansContext = struct.pack('!I25sI',Size,str(IP),ListenPort)
+                else:
+                    ansType = AnswerType.failed
+                    ansContext = struct.pack('!100s','File Owner Logout!!!!!!!')
+            return ansType, ansContext
+        
+if __name__ == '__main__':
+    server = Server()
+    server.initServer()
+    #server.addUser('dc','ddd')
+    #print server.getUser(1)
+    #server.delUser('dc')
+    #server.userLogin('dc','ddd','127.0.0.1',8888,7000)
+    #server.getUser(1)
+    #server.addFile('1.txt',20,1)
+    #server.addFile('3.txt',20,1)
+    #server.addFile('2.txt',20,1)
+    #server.delFile('3.txt',1)
+    #info = server.getFile(1)
+    localIP = socket.gethostbyname(socket.gethostname())
+    server.startServer(localIP)
+    
+    
